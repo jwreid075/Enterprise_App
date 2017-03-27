934bd5095c15f9f196198fe4fd2acd951e7685ce
Jeremiah Reid <jwr17@students.pti.edu>
2017-03-27 14:38:12 -0400

    Add files via upload

--- JavaApplication1/src/TestServ.java
+++ JavaApplication1/src/TestServ.java
@@ -35,8 +35,11 @@ public class TestServ extends HttpServlet
                 pw.print(htmlPrintTwo);
                 pw.print("<body>\n" +"\n" +"<h1>Create your order</h1>\n" +"\n" +"<div style=\"Width:100%\">\n" +
                         "<table  align=\"left\" style=\"border:2px double black; width:40%;\">");
-               pw.print("<script> function AddToOrder(var id: string)"
-                        + "{document.getElementById(\"newField\" + id).innerHTML = \"1\"}</script>");
+               pw.print("<script>"
+                       + "var btnArray = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];"
+                       + "var nameArray = new Array(14);"
+                       + "var priceArray = new Array(14);"
+                       + "</script>");
                  
                while(rs.next())
                 {
@@ -48,12 +51,28 @@ public class TestServ extends HttpServlet
                     /*pw.print("<center>" + foodName + " " + foodPrice + " " + "<button type=\"button\">"
                             + "Add to order</button><hr/></center>");*/  
                     pw.print("<tr>\n" +"<td style=\"border:2px double black;\">" + foodName + "</td>\n" +"<td style=\"border:2px double black;\">" + 
-                            foodPrice + "</td>\n" +"<td style=\"border:2px double black;\"><button type=\"button\" onClick=\"AddToOrder(" + idNum + ")\" id=\"b" + idNum + "\">"+ "Add to order</button></td>\n" +"</tr>");
+                            foodPrice + "</td>\n" +"<td style=\"border:2px double black;\"><button type=\"button\" onClick=\"AddToOrder(" + idNum + ")\" id=\"b" + idNum + "\">"+ "Add to order</button></td>\n<td><p id=\"newField" + idNum + "\">0</p></td>" +"</tr>");
+                    pw.print("<script>document.getElementById(\"b" + idNum + "\").addEventListener(\"click\",AddToOrder);</script>");
+                    pw.print("<script>nameArray[" + (idNum-1) + "] = \"" + foodName + "\";</script>");
+                    pw.print("<script>priceArray[" + (idNum-1) + "] = " + foodPrice + ";</script>");
+
                     idNum++;
                 }
-                
                 pw.print("</table>");
-                pw.print("<p id=\"newField3\"></p>");
+                pw.print("<script>"
+                       + "function AddToOrder(id)"
+                       + "{"
+                       + "btnArray[id - 1] += 1;"
+                       + "var output = btnArray[id - 1].toString();"
+                       + "document.getElementById(\"newField\" + id).innerHTML = output;}"
+                       + "function CompleteOrder(orderID)"
+                       + "{"
+                       +  ""
+                       + "}"
+                       + "</script>");
+                
+                pw.print("<form action=\"home.html\"><input type =\"submit\" value=\"Home\"/></form></li>");
+                pw.print("</body>");
                 //}
                 //pw.println(rs.getInt(1) + " " + rs.getString(2)+ " " +rs.getString(3));
                 con.close();
